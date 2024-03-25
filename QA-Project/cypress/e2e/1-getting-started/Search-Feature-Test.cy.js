61f9f802e09813512bf77692d67992dd4ddf3a30
zahaaMuhanna <106498282+zahaaMuhanna@users.noreply.github.com>
2024-03-26 00:24:39 +0200

    adding test cases

--- QA-Project/cypress/e2e/1-getting-started/Search-Feature-Test.cy.js
+++ QA-Project/cypress/e2e/1-getting-started/Search-Feature-Test.cy.js
@@ -24,6 +24,35 @@ describe('Search Feature Tests', () => {
         cy.get('input[class="search-bar"]').type('React 11{enter}'); 
         cy.get('.card').should('exist');
     });
+    it('TC-05: Search for a multiple keywords', () => {
+        cy.get('input[class="search-bar"]').type('JavaScript React Node.js{enter}'); 
+        cy.get('.card').should('exist');
+    });
+  
+    // Invalid Test Cases
+    it('TC-06: Search for an empty string', () => {
+        cy.get('input[class="search-bar"]').type('{enter}'); 
+        cy.get('.Not-found-container').should('exist'); 
+    });
+  
+    it('TC-07: Search for a non-existent keyword', () => {
+        cy.get('input[class="search-bar"]').type('XYZ{enter}'); 
+        cy.get('.Not-found-container').should('exist'); 
+    });
+    it('TC-08: Search for an invalid company name', () => {
+        cy.get('input[class="search-bar"]').type('Black Rock{enter}'); 
+        cy.get('.Not-found-container').should('exist'); 
+    });
+
+    it('TC-09: Search for a category with invalid characters', () => {
+        cy.get('input[class="search-bar"]').type('Web Development!#{enter}'); 
+        cy.get('.Not-found-container').should('exist'); 
+    });
+
+    it('TC-10: Search for a multiple invalid keywords', () => {
+        cy.get('input[class="search-bar"]').type('XYZ123!@{enter}'); 
+        cy.get('.Not-found-container').should('exist'); 
+    });
 
 });
 
